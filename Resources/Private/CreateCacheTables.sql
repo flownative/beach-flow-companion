59d2b22ed52f0ad3de8e0eef11e5936000db0fc6
Karsten Dambekalns <karsten@dambekalns.de>
2019-03-01 10:25:44 +0100

    TASK: Adjust cache table schema to official one

--- Resources/Private/CreateCacheTables.sql
+++ Resources/Private/CreateCacheTables.sql
@@ -4,17 +4,17 @@ CREATE TABLE IF NOT EXISTS "cache" (
   "identifier" VARCHAR(250) NOT NULL,
   "cache" VARCHAR(250) NOT NULL,
   "context" VARCHAR(150) NOT NULL,
-  "created" INTEGER UNSIGNED NOT NULL,
+  "created" INTEGER UNSIGNED DEFAULT '0' NOT NULL,
   "lifetime" INTEGER UNSIGNED DEFAULT '0' NOT NULL,
-  "content" LONGTEXT,
+  "content" LONGTEXT NOT NULL,
   PRIMARY KEY ("identifier", "cache", "context")
 ) ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS "tags" (
-  "identifier" VARCHAR(250) NOT NULL,
+  "identifier" VARCHAR(255) NOT NULL,
   "cache" VARCHAR(250) NOT NULL,
   "context" VARCHAR(150) NOT NULL,
-  "tag" VARCHAR(250) NOT NULL,
+  "tag" VARCHAR(255) NOT NULL,
   INDEX "identifier" ("identifier", "cache", "context"),
   INDEX "tag" ("tag")
 ) ENGINE = InnoDB;
